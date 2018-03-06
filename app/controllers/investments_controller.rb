41a66d4369b180c171ca459fa2830c5c083f2629
SomeMissingColon <thomasnoidion@gmail.com>
2018-03-06 16:58:30 -0500

    done investment controller

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,22 +1,40 @@
 class InvestmentsController < ApplicationController
   def index
+    #foundation projects where they have investments
+        #total funding with projects
+        #project name
+        #next milestone time left
+        #project description
+        #edit button => edit form
   end
 
   def new
+    #form
   end
 
   def create
+    # normal
   end
 
   def edit
+    #normal
   end
 
   def update
+    #normal
   end
 
   def show
+    #milestones for project
+        #amount for milestone
+        #task
+        #time due
+        #time left
+        #total amount if all unlocked
+        #currently gaved
   end
 
   def destroy
   end
+
 end
