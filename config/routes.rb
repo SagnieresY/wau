cd0bd1605b13c42c2f0d5df3875a6bd7647b05a1
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-03-06 13:51:00 -0500

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :investements do
+    resources :milestones, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :projects, only: [:show, :new, :create, :edit, :update]
+  get '/dashboard', to: 'pages#dashboard', as: :dashboard
 end
