6bf6c8c91c229aceea3e60c7dd45e39279a32388
SomeMissingColon <thomasnoidion@gmail.com>
2018-03-06 17:48:14 -0500

    completed pseudocode for investment and milestone model

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -4,4 +4,7 @@ class Investment < ApplicationRecord
   has_many :milestones
   validates :project, presence: true
   validates :foundation, presence: true
+  def projected_amount
+    #calculates projected amount minus the missed milestones
+  end
 end
