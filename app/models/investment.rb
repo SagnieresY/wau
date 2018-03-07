97fa2ddef23879355c93a7856e2a1d6723b7bc5c
SomeMissingColon <thomasnoidion@gmail.com>
2018-03-07 11:17:49 -0500

    projected amount method done

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -6,5 +6,9 @@ class Investment < ApplicationRecord
   validates :foundation, presence: true
   def projected_amount
     #calculates projected amount minus the missed milestones
+    valid_milestones = milestones.map do |m| #check if deadline passed or if the milestone task was done
+      Date.today > m.deadline || m.unlocked ? 0 : m.amount
+    end
+    valid_milestones.reduce(0, :+)
   end
 end
