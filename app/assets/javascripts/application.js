2f5dd4f9be4652713fe780f026e2d5b6c12fad44
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-03-12 11:32:40 -0400

    added jQuery sorter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,5 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+
 //= require rails-ujs
+//= require jquery
 //= require_tree .
+
+
