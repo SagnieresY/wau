09494787418d2f125eccbe020b2be8f85e217676
Damien Guillot <damien.guillot@edu.ebs-paris.com>
2018-03-14 11:44:47 -0400

    Logic footer, navbar error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -17,4 +16,7 @@ Rails.application.routes.draw do
   post '/unlock_milestone', to: 'milestones#unlock', as: :unlock_milestone
   post '/rescind_milestone', to: 'milestones#rescind', as: :rescind_milestone
   post '/investment_unlocked_amount', to: 'investments#unlocked_amount', as: :investment_unlocked_amount
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
