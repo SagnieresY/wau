a2877876ea9d20f77e24f05426f013b4edc0fbe1
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-03-22 18:55:09 -0400

    all EN.yml added for all page + forms etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
 
   scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users
     root to: 'pages#home'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
