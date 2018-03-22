8439232cc65363600a68ff40f29b38023c427ead
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-03-22 19:01:32 -0400

    only FR and EN  local

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+  scope '(:locale)', locale: /fr/ do
     devise_for :users
     root to: 'pages#home'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
