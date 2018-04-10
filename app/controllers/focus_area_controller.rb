cc9b0553a297d0119d60d448a3062855e9f8e2f8
SomeMissingColon <thomasnoidion@gmail.com>
2018-04-10 11:40:26 -0400

    added reset button for searchs

--- app/controllers/focus_area_controller.rb
+++ app/controllers/focus_area_controller.rb
@@ -5,6 +5,7 @@ class FocusAreaController < ApplicationController
     FocusArea.search_by_name(params[:query]).uniq.each_with_index do |focus, i|
       @focus_areas[:results].push(focus.name)
     end
+    @focus_areas[:results] = @focus_areas[:results].uniq
     render json: @focus_areas
    end
 end
