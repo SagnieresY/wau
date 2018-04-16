6f074857385d96d53c2dc4d18f349d5fc5fb1699
SomeMissingColon <thomasnoidion@gmail.com>
2018-04-16 10:57:17 -0400

    finished user testing

--- test/models/focus_area_test.rb
+++ test/models/focus_area_test.rb
@@ -4,4 +4,5 @@ class FocusAreaTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,11 +8,26 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save
   end
 
+  test 'does not save an user without email' do
+    user = User.new(password:'123456')
+    assert_not user.save
+  end
+
+  test 'does not save an user without password' do
+    user = User.new(email:'thomas@email.com')
+    assert_not user.save
+  end
+
   test 'returns array of attributes' do
     user = User.create!(email:'test@test.com',password:123456)
     user_id = user.id
     assert user.to_a == [user_id,user.email,user.created_at]
   end
 
+  test 'can delete user' do
+    user = User.create(email:'test1@test.com',password:'123456')
+    user_id = user.id
+    assert user.destroy
+  end
 
 end
