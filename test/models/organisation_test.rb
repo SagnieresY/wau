9f909252c46d7b3513a02fa6fa80addf1f5de44a
SomeMissingColon <thomasnoidion@gmail.com>
2018-04-16 16:37:32 -0400

    locked amoutn testing organisaiton

--- test/models/organisation_test.rb
+++ test/models/organisation_test.rb
@@ -4,6 +4,15 @@ class OrganisationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @test_org = Organisation.create(name:'testing')
+    @test_focus = FocusArea.create!(name:'test')
+    @test_geo = Geo.create(name:'test land')
+    @test_project = Project.create!(name:'test',description:'testing',focus_area: @test_focus,main_contact:'www@www.com',organisation: @test_org, geos:[@test_geo])
+    @test_investment = Investment.create!(organisation:@test_org,project:@test_project)
+
+  end
+
   test "doesnt save without a name" do
     assert_not Organisation.new.save
   end
@@ -21,12 +30,19 @@ class OrganisationTest < ActiveSupport::TestCase
     test_org = Organisation.create!(name:'test')
     focus = FocusArea.create!(name:'test')
     geo = Geo.create(name:'test land')
-    project = Project.create!(name:'test',description:'testing',focus_area: focus,main_contact:'www@www.com',organisation: test_org, geos:[geo])
+    project = Project.create!(name:'test',description:'testing',focus_area: focus,main_contact:'www@www.com',organisation: test_org, geos:[@test_geo])
 
     Investment.create!(organisation: test_org, project: project)
 
     assert test_org.destroy
   end
 
+  test "returns correct total locked amount" do
+    10.times do
+      Installment.create(amount:100, task:'meme',deadline:Date.today,investment:@test_investment)
+    end
+
+    assert @test_org.locked_amount == 100*10
+  end
 
 end
